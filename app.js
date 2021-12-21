100831f3d8d4e41c6c715a329e011def8cefe4ac
haneulffer98 <76591804+haneulffer98@users.noreply.github.com>
2021-12-21 12:32:26 +0700

    added js

---
+++ app.js
@@ -0,0 +1,26 @@
+const countdown = () => {
+  const countDate = new Date("January 1, 2022 00:32:00").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  document.querySelector(".days").innerText = textDay;
+  document.querySelector(".hours").innerText = textHour;
+  document.querySelector(".minutes").innerText = textMinute;
+  document.querySelector(".sec").innerText = textSecond;
+
+  if (gap < 10000) {
+    launch();
+  }
+};
+
+setInterval(countdown, 1000);
